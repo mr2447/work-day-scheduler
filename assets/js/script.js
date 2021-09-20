edd9f7ceef43fe0ad0c391b7ba5a8213822ddf19
Marvin Ren <marvin.ren@yale.edu>
2021-09-20 15:34:15 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 //declare a "toDo" cariable that holds the parsed to-do items retrieved from localStorage
 //if there is nothing in localStorag, set the list to an empty array
-var toDo = JSON.parse(localStorage.getItem('to-do')) || [];
+var toDo = JSON.parse(localStorage.getItem('to-dos') || [])
 
-//Render out to-do to the page
-function renderToDos (toDo) {
-    //Iterates over the 'toDo'
-    for (var i = 0; i < toDo.length; i++) {
-        var hour9 = $("<textarera>")
-    }
+$('#add-to-do').on('click', function () {
+
+
 }
