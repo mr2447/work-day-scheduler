b7d4e111db41b3a543ff0fb15a2cbb85c91738b0
Marvin Ren <marvin.ren@yale.edu>
2021-09-20 15:53:13 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-//declare a "toDo" cariable that holds the parsed to-do items retrieved from localStorage
-//if there is nothing in localStorag, set the list to an empty array
-var toDo = JSON.parse(localStorage.getItem('to-dos') || [])
+$("#add-9").on("click", function (event) {
+    event.preventDefault();
 
-$('#add-to-do').on('click', function () {
-
-
-}
+    //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
+    var hour9Task = $("#hour-9").val().trim();
+    console.log(hour9Task);
+})
