7d413479613c914703f8fb63df7bc71ca55f5b99
Marvin Ren <marvin.ren@yale.edu>
2021-09-20 20:06:57 -0400

    local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-$("#add-9").on("click", function (event) {
-    event.preventDefault();
+var nineContent = localStorage.getItem("hourNine") || "";
 
+function renderNine(nineContent) {
+    var toDoItem = $('<textarea>');
+    console.log(toDoItem);
+    toDoItem.text(nineContent);
+}
+
+$("#addNine").on("click", function (event) {
+    event.preventDefault();
+   
     //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
-    var hour9Task = $("#hour-9").val().trim();
-    console.log(hour9Task);
+    var hour9Task = $("#hourNine").val().trim();
+    localStorage.setItem("hourNine", hour9Task);
+    renderNine(nineContent);
 })
