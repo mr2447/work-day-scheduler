ef826524cd120d0c4778d4c4a6c545e7dab71104
Marvin Ren <marvin.ren@yale.edu>
2021-09-28 15:13:55 -0400

    compare

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,25 @@
-//Hour 9 code:
-var nineContent = localStorage.getItem("hourNine") || "";
-
-function renderNine(nineContent) {
-    $("#hourNine").val(nineContent);
-}
-
-$("#addNine").on("click", function (event) {
-    event.preventDefault();
-    
-    //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
-    var hour9Task = $("#hourNine").val().trim();
-    localStorage.setItem("hourNine", hour9Task);
-    
+//save button to localStorage text for every hour
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    // store key-value pair: Key= ex.hour-9, value= textarea value
+    localStorage.setItem(hour, text)
 })
 
-renderNine(nineContent);
 
 var thisHour = moment().format("H");
-
 var hourNine = $("#hour-nine").text().trim();
-console.log(hourNine);
-console.log(thisHour);
-if (hourNine === thisHour) {
-    $("#hourNine").addClass("p-3 mb-2 bg-success text-white");
-    console.log("its 9 and this is green")
-} else if (hourNine > thisHour){
-    $("#hourNine").addClass("p-3 mb-2 bg-transparent text-dark");
-    console.log ("its in the future and this is colorless")
-} else {
-    $("#hourNine").addClass("p-3 mb-2 bg-danger text-white");
-    console.log ("its in the past and this is red")
-}
-
-//Hour 10
-var tenContent = localStorage.getItem("hourTen") || "";
-
-function renderTen(tenContent) {
-    $("#hourTen").val(tenContent);
-}
-
-$("#addTen").on("click", function (event) {
-    event.preventDefault();
+function auditTask () {
+    if (thisHour.isAfter(hourNine)) {
     
-    //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
-    var hour10Task = $("#hourTen").val().trim();
-    localStorage.setItem("hourTen", hour10Task);
-    
-})
-
-renderTen(tenContent);
-
-
-
-var hourTen = $("#hour-ten").text().trim();
-console.log(hourTen);
-console.log(thisHour);
-if (hourTen === thisHour) {
-    $("#hourTen").addClass("p-3 mb-2 bg-success text-white");
-    console.log("its 10 and this is green")
-} else if (hourNine > thisHour){
-    $("#hourTen").addClass("p-3 mb-2 bg-transparent text-dark");
-    console.log ("its in the future and this is colorless")
-} else {
-    $("#hourTen").addClass("p-3 mb-2 bg-danger text-white");
-    console.log ("its in the past and this is red")
+        console.log("its after")
+    }
 }
 
-//hour 11
+//render text from local storage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+
+
---
+++ assets/js/testing.js
@@ -0,0 +1,80 @@
+//Hour 9 code:
+var nineContent = localStorage.getItem("hourNine") || "";
+
+function renderNine(nineContent) {
+    $("#hourNine").val(nineContent);
+}
+//==========================
+//save button to localStorage text for every hour
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, text)
+})
+//render text from local storage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+
+//==========================
+$("#addNine").on("click", function (event) {
+    event.preventDefault();
+    
+    //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
+    var hour9Task = $("#hourNine").val().trim();
+    localStorage.setItem("hourNine", hour9Task);
+    
+})
+
+renderNine(nineContent);
+
+var thisHour = moment().format("H");
+
+var hourNine = $("#hour-nine").text().trim();
+console.log(hourNine);
+console.log(thisHour);
+if (hourNine === thisHour) {
+    $("#hourNine").addClass("p-3 mb-2 bg-success text-white");
+    console.log("its 9 and this is green")
+} else if (hourNine > thisHour){
+    $("#hourNine").addClass("p-3 mb-2 bg-transparent text-dark");
+    console.log ("its in the future and this is colorless")
+} else {
+    $("#hourNine").addClass("p-3 mb-2 bg-danger text-white");
+    console.log ("its in the past and this is red")
+}
+
+//Hour 10
+var tenContent = localStorage.getItem("hourTen") || "";
+
+function renderTen(tenContent) {
+    $("#hourTen").val(tenContent);
+}
+
+$("#addTen").on("click", function (event) {
+    event.preventDefault();
+    
+    //Get the hour-9 value from the textarea and store it as a variable using .val() and .trim()
+    var hour10Task = $("#hourTen").val().trim();
+    localStorage.setItem("hourTen", hour10Task);
+    
+})
+
+renderTen(tenContent);
+
+
+
+var hourTen = $("#hour-ten").text().trim();
+console.log(hourTen);
+console.log(thisHour);
+if (hourTen === thisHour) {
+    $("#hourTen").addClass("p-3 mb-2 bg-success text-white");
+    console.log("its 10 and this is green")
+} else if (hourNine > thisHour){
+    $("#hourTen").addClass("p-3 mb-2 bg-transparent text-dark");
+    console.log ("its in the future and this is colorless")
+} else {
+    $("#hourTen").addClass("p-3 mb-2 bg-danger text-white");
+    console.log ("its in the past and this is red")
+}
+
+//hour 11
